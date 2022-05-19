9799d745192e7b48e24d449bca1aad091f284647
Yuxin Ye <yuxin_ye@protonmail.com>
2022-05-19 11:01:36 +1000

    set up movie component

---
+++ src/components/movies.jsx
@@ -0,0 +1,50 @@
+import { render } from "@testing-library/react";
+import { getMovies } from "../services/fakeMovieService";
+import React, { Component } from "react";
+
+// Used OOD for now
+class Movies extends Component {
+  // Not standard initilisation
+  state = {
+    movies: getMovies(),
+  };
+
+  handleDelete = (movie) => {
+    console.log(movie);
+  };
+
+  render() {
+    return (
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Genre</th>
+            <th>Stock</th>
+            <th>Rate</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.state.movies.map((movie) => (
+            <tr key={movie._id}>
+              <td>{movie.title}</td>
+              <td>{movie.genre.name}</td>
+              <td>{movie.numberInStock}</td>
+              <td>{movie.dailyRentalRate}</td>
+              <td>
+                <button
+                  onClick={() => this.handleDelete(movie)}
+                  className="btn btn-danger btn-sm"
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+export default Movies;
