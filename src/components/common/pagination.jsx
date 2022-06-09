92a38fb57ef8608c1abf077012a958e42e485fa0
Yuxin Ye <yuxin_ye@protonmail.com>
2022-06-09 11:46:19 +1000

    use lodash to calculate the pages and render in the pagniation section

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import _ from "lodash";
 
 const Pagination = (props) => {
-  return null;
+  const { itemsCount, pageSize } = props;
+  // use the props to calculate the number of pages
+  const pagesCount = itemsCount / pageSize;
+  const pagesArray = _.range(1, pagesCount + 1);
+
+  return (
+    <nav aria-label="Page navigation example">
+      <ul className="pagination">
+        {pagesArray.map((pageItem) => (
+          <li key={pageItem} className="page-item">
+            <a className="page-link" href="#">
+              {pageItem}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 };
 
 export default Pagination;
