f92618f241a80b68d7361c8034d64d74f3a8e03e
Yuxin Ye <yuxin_ye@protonmail.com>
2022-06-09 11:48:40 +1000

    add a condition to remove pagination when only one page present

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -5,15 +5,18 @@ const Pagination = (props) => {
   const { itemsCount, pageSize } = props;
   // use the props to calculate the number of pages
   const pagesCount = itemsCount / pageSize;
+  //  not to use pagination when there's only one page
+  if (pagesCount === 1) {
+    return null;
+  }
   const pagesArray = _.range(1, pagesCount + 1);
-
   return (
     <nav aria-label="Page navigation example">
       <ul className="pagination">
-        {pagesArray.map((pageItem) => (
-          <li key={pageItem} className="page-item">
+        {pagesArray.map((pageNumber) => (
+          <li key={pageNumber} className="page-item">
             <a className="page-link" href="#">
-              {pageItem}
+              {pageNumber}
             </a>
           </li>
         ))}
