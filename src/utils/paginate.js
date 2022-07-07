d325fcb3c0d3478e1a690fc1573c302009e76861
Yuxin Ye <yuxin_ye@protonmail.com>
2022-07-07 11:55:21 +1000

    use better variable names

--- src/utils/paginate.js
+++ src/utils/paginate.js
@@ -1,6 +1,6 @@
 import _ from "lodash";
 
-export function paginate(items, pageNumber, pageSize) {
-  const startIndex = (pageNumber - 1) * pageSize;
+export function paginate(items, currPage, pageSize) {
+  const startIndex = (currPage - 1) * pageSize;
   return _(items).slice(startIndex).take(pageSize).value();
 }
