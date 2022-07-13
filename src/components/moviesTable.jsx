1991a6427262d7c0786a2a37e7f28f7a2e6c8815
Yuxin Ye <yuxin_ye@protonmail.com>
2022-07-13 10:50:58 +1000

    change moviesTable into a class component

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,58 +1,61 @@
-import React from "react";
+import React, { Component } from "react";
 import LoveHeart from "../common/LoveHeart";
-const MoviesTable = (props) => {
-  const { paginatedMovies, onDelete, onLike, onSort } = props;
-  return (
-    <table className="table">
-      <thead>
-        <tr>
-          <th
-            style={{ cursor: "pointer" }}
-            scope="col"
-            onClick={() => onSort("title")}
-          >
-            Title
-          </th>
-          <th
-            style={{ cursor: "pointer" }}
-            scope="col"
-            onClick={() => onSort("genre.name")}
-          >
-            Genre
-          </th>
-          <th
-            style={{ cursor: "pointer" }}
-            scope="col"
-            onClick={() => onSort("dailyRentalRate")}
-          >
-            Ratings
-          </th>
-          <th scope="col">Likes</th>
-          <th scope="col">Edits</th>
-        </tr>
-      </thead>
-      <tbody>
-        {paginatedMovies.map((movie) => (
-          <tr key={movie._id}>
-            <td>{movie.title}</td>
-            <td>{movie.genre.name}</td>
-            <td>{movie.dailyRentalRate}</td>
-            <td>
-              <LoveHeart loved={movie.liked} onClick={() => onLike(movie)} />
-            </td>
-            <td>
-              <button
-                className="btn btn-danger btn-sm"
-                onClick={() => onDelete(movie)}
-              >
-                Delete
-              </button>
-            </td>
+
+class MoviesTable extends Component {
+  render() {
+    const { paginatedMovies, onDelete, onLike, onSort } = this.props;
+    return (
+      <table className="table">
+        <thead>
+          <tr>
+            <th
+              style={{ cursor: "pointer" }}
+              scope="col"
+              onClick={() => onSort("title")}
+            >
+              Title
+            </th>
+            <th
+              style={{ cursor: "pointer" }}
+              scope="col"
+              onClick={() => onSort("genre.name")}
+            >
+              Genre
+            </th>
+            <th
+              style={{ cursor: "pointer" }}
+              scope="col"
+              onClick={() => onSort("dailyRentalRate")}
+            >
+              Ratings
+            </th>
+            <th scope="col">Likes</th>
+            <th scope="col">Edits</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
-  );
-};
+        </thead>
+        <tbody>
+          {paginatedMovies.map((movie) => (
+            <tr key={movie._id}>
+              <td>{movie.title}</td>
+              <td>{movie.genre.name}</td>
+              <td>{movie.dailyRentalRate}</td>
+              <td>
+                <LoveHeart loved={movie.liked} onClick={() => onLike(movie)} />
+              </td>
+              <td>
+                <button
+                  className="btn btn-danger btn-sm"
+                  onClick={() => onDelete(movie)}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
+}
 
 export default MoviesTable;
