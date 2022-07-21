a820e53eccb9d86cee6c8da158fa86e3333a24f5
Yuxin Ye <yuxin_ye@protonmail.com>
2022-07-22 09:59:53 +1000

    add page navigation for not found page

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import NavBar from "./components/navBar";
 import Customers from "./components/customers";
 import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 
 class App extends Component {
   render() {
@@ -14,10 +14,12 @@ class App extends Component {
         <NavBar />
         <main className="container">
           <Routes>
+            <Route path="/" element={<Movies />} />
             <Route path="/movies" element={<Movies />} />
             <Route path="/rentals" element={<Rentals />} />
             <Route path="/customers" element={<Customers />} />
-            <Route path="*" element={<NotFound />} />
+            <Route path="/not-found" element={<NotFound />} />
+            <Route path="*" element={<Navigate to="/not-found" />} />
           </Routes>
         </main>
       </>
