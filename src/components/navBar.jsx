886f39f858fc0c7f85d8307882b3b9ea661c0aae
Yuxin Ye <yuxin_ye@protonmail.com>
2022-07-21 16:55:45 +1000

    add navLinks to the navBar

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,34 +1,41 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <nav class="mb-4 px-3 navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">
-        Navbar
-      </a>
+    <nav class="mb-4 px-4 navbar navbar-expand-lg navbar-light bg-light">
+      <Link to="/movies" className="navbar-brand">
+        MovieApp
+      </Link>
       <button
         class="navbar-toggler"
         type="button"
         data-toggle="collapse"
-        data-target="#navbarNavAltMarkup"
-        aria-controls="navbarNavAltMarkup"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
         aria-expanded="false"
         aria-label="Toggle navigation"
       >
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-          <a class="nav-item nav-link active" href="#">
-            Home <span class="sr-only">(current)</span>
-          </a>
-          <a class="nav-item nav-link" href="#">
-            Features
-          </a>
-          <a class="nav-item nav-link" href="#">
-            Pricing
-          </a>
-        </div>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <Link to="/movies" className="nav-link">
+              Movies
+            </Link>
+          </li>
+          <li class="nav-item">
+            <Link to="/customers" className="nav-link">
+              Customers
+            </Link>
+          </li>
+          <li class="nav-item">
+            <Link to="/rentals" className="nav-link">
+              Rentals
+            </Link>
+          </li>
+        </ul>
       </div>
     </nav>
   );
