6c9a157f4e4d7b54b0172afd872ffb2c027a21bd
Yuxin Ye <yuxin_ye@protonmail.com>
2022-07-21 16:38:56 +1000

    add boostrap look for the nabbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from "react";
 import "./App.css";
 import Movies from "./components/movies";
+import NavBar from "./components/navBar";
 
 class App extends Component {
   render() {
     return (
-      <main className="container">
-        <Movies />
-      </main>
+      <>
+        <NavBar />
+        <main className="container">
+          <Movies />
+        </main>
+      </>
     );
   }
 }
