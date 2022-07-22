bd6fb8445e1b61b099c39176a74c2ea9507c0a68
Yuxin Ye <yuxin_ye@protonmail.com>
2022-07-22 16:09:57 +1000

    controlled state

---
+++ src/components/loginForm.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from "react";
+
+class LoginForm extends Component {
+  state = {
+    account: { username: "", password: "" },
+  };
+
+  // Note: minimise the use of Ref
+  username = React.createRef();
+
+  // This can be replaced by autoFocus attribute
+  // componentDidMount() {
+  //   this.username.current.focus();
+  // }
+  handleChange = (e) => {
+    const account = { ...this.state.account };
+    account.username = e.currentTarget.value;
+    this.setState({ account });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    // Get the value from input fields
+    const username = this.username.current.value;
+  };
+  render() {
+    return (
+      <div>
+        <h1>Login</h1>
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="username" className="mt-4">
+              Username
+            </label>
+            <input
+              // controlled state
+              value={this.state.account.username}
+              onChange={this.handleChange}
+              autoFocus
+              placeholder="User name"
+              id="username"
+              type="text"
+              className="mt-2 form-control"
+              ref={this.username}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password" className="mt-4">
+              Password
+            </label>
+            <input
+              placeholder="Password"
+              id="password"
+              type="text"
+              className="mt-2 form-control"
+            />
+          </div>
+          <button className="mt-4 btn btn-primary">Login</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
